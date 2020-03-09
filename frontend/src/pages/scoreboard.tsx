683b39147920b68fe8e20c0288719c882a736c6e
lukasmoellerch <mail@lukas-moeller.ch>
2020-03-09 01:04:19 +0100

    Fix scoreboard dark mode

--- frontend/src/pages/scoreboard.tsx
+++ frontend/src/pages/scoreboard.tsx
@@ -4,6 +4,7 @@ import { UserInfo } from "../interfaces";
 import { fetchapi } from "../fetch-utils";
 import { Link } from "react-router-dom";
 import globalcss from "../globalcss";
+import { Card } from "../components/card";
 
 const styles = {
   wrapper: css({
@@ -29,7 +30,6 @@ const styles = {
   }),
   scrollOverflow: css({
     overflowX: "auto",
-    boxShadow: "grey 0px 2px 4px 0px",
   }),
 };
 
@@ -84,63 +84,65 @@ export default class Scoreboard extends React.Component<Props, State> {
       <div {...styles.wrapper}>
         {this.state.error && <p>{this.state.error}</p>}
         <h1>Scoreboard</h1>
-        <div {...styles.scrollOverflow}>
-          <table {...styles.scoreboardTable}>
-            <thead>
-              <tr {...styles.header}>
-                <th>Rank</th>
-                <th>User</th>
-                <th
-                  {...styles.canClick}
-                  onClick={() => this.loadScoreboard("score")}
-                >
-                  Score
-                </th>
-                <th
-                  {...styles.canClick}
-                  onClick={() => this.loadScoreboard("score_answers")}
-                >
-                  Answers
-                </th>
-                <th
-                  {...styles.canClick}
-                  onClick={() => this.loadScoreboard("score_comments")}
-                >
-                  Comments
-                </th>
-                <th
-                  {...styles.canClick}
-                  onClick={() => this.loadScoreboard("score_cuts")}
-                >
-                  Import Exams
-                </th>
-                <th
-                  {...styles.canClick}
-                  onClick={() => this.loadScoreboard("score_legacy")}
-                >
-                  Import Wiki
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.scoreboard.map((board, idx) => (
-                <tr key={board.username} {...styles.row}>
-                  <td>{idx + 1}</td>
-                  <td {...globalcss.noLinkColor}>
-                    <Link to={"/user/" + board.username}>
-                      {board.displayName}
-                    </Link>
-                  </td>
-                  <td>{board.score}</td>
-                  <td>{board.score_answers}</td>
-                  <td>{board.score_comments}</td>
-                  <td>{board.score_cuts}</td>
-                  <td>{board.score_legacy}</td>
+        <Card>
+          <div {...styles.scrollOverflow}>
+            <table {...styles.scoreboardTable}>
+              <thead>
+                <tr {...styles.header}>
+                  <th>Rank</th>
+                  <th>User</th>
+                  <th
+                    {...styles.canClick}
+                    onClick={() => this.loadScoreboard("score")}
+                  >
+                    Score
+                  </th>
+                  <th
+                    {...styles.canClick}
+                    onClick={() => this.loadScoreboard("score_answers")}
+                  >
+                    Answers
+                  </th>
+                  <th
+                    {...styles.canClick}
+                    onClick={() => this.loadScoreboard("score_comments")}
+                  >
+                    Comments
+                  </th>
+                  <th
+                    {...styles.canClick}
+                    onClick={() => this.loadScoreboard("score_cuts")}
+                  >
+                    Import Exams
+                  </th>
+                  <th
+                    {...styles.canClick}
+                    onClick={() => this.loadScoreboard("score_legacy")}
+                  >
+                    Import Wiki
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {this.state.scoreboard.map((board, idx) => (
+                  <tr key={board.username} {...styles.row}>
+                    <td>{idx + 1}</td>
+                    <td {...globalcss.noLinkColor}>
+                      <Link to={"/user/" + board.username}>
+                        {board.displayName}
+                      </Link>
+                    </td>
+                    <td>{board.score}</td>
+                    <td>{board.score_answers}</td>
+                    <td>{board.score_comments}</td>
+                    <td>{board.score_cuts}</td>
+                    <td>{board.score_legacy}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </Card>
       </div>
     );
   }
