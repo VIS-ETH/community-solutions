ee6b2bced7f2cca38b92018b8189db31f4dff5c9
kelmich <kelmich@student.ethz.ch>
2022-04-03 13:39:02 +0000

    Initial Fix Attempt

--- frontend/src/pdf/pdfjs.js
+++ frontend/src/pdf/pdfjs.js
@@ -1,14 +1,8 @@
-/**
- * By default worker-loader won't output to /static. Therefore we need to reexport
- * pdfjs and inject the correct location using webpack's loader sytnax. I hope this
- * is just a temporary workaround because there probably is a way to do this nicely.
- * We could also overwrite the webpack config...
- */
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import PdfjsWorker from "worker-loader?publicPath=/static/!pdfjs-dist/build/pdf.worker.js";
-import { GlobalWorkerOptions } from "pdfjs-dist";
+const pdfjs = require("pdfjs-dist/build/pdf.js");
+const PdfjsWorker = require("worker-loader?esModule=false&filename=[name].[contenthash].js!pdfjs-dist/build/pdf.worker.js");
 
 if (typeof window !== "undefined" && "Worker" in window) {
-  GlobalWorkerOptions.workerPort = new PdfjsWorker();
+  pdfjs.GlobalWorkerOptions.workerPort = new PdfjsWorker();
 }
-export * from "pdfjs-dist";
+
+module.exports = pdfjs;
