ecb828be29fa5059e0726e9ceeed39a84310d7c2
Luca Schnellmann <lschnellman@ethz.ch>
2026-06-13 15:31:37 +0200

    Migrate documents API to Django Ninja

--- backend/util/middleware.py
+++ backend/util/middleware.py
@@ -6,16 +6,20 @@ from django.http.multipartparser import MultiPartParser
 def parse_request_middleware(get_response):
     def middleware(request):
         if request.method == "PUT" or request.method == "PATCH":
-            try:
-                parser = MultiPartParser(
-                    request.META, BytesIO(request.body), request.upload_handlers
-                )
-                request.DATA, files = parser.parse()
-                request.FILES.update(files)
-            except Exception:
-                import traceback
+            # django-ninja handles PUT/PATCH
+            if not request.path.startswith(
+                ("/api/document/", "/api/feedback/", "/api/image/")
+            ):
+                try:
+                    parser = MultiPartParser(
+                        request.META, BytesIO(request.body), request.upload_handlers
+                    )
+                    request.DATA, files = parser.parse()
+                    request.FILES.update(files)
+                except Exception:
+                    import traceback
 
-                traceback.print_exc()
+                    traceback.print_exc()
         elif request.method == "POST":
             request.DATA = request.POST
         return get_response(request)
