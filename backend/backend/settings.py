6a89ec4baed88d54931c9750e24fd2ad0c11cd2f
Luca Schnellmann <lschnellman@ethz.ch>
2026-07-11 10:06:44 +0200

    Fix middleware ordering and body parsing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -277,9 +277,9 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "csp.middleware.CSPMiddleware",
+    "ninja.compatibility.files.fix_request_files_middleware",
     "util.middleware.parse_request_middleware",
     "django_prometheus.middleware.PrometheusAfterMiddleware",
-    "ninja.compatibility.files.fix_request_files_middleware",
 ]
 
 if DEBUG and not TESTING:
