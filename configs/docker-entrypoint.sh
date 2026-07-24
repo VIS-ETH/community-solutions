b378c468e87c8c221c4ea59980b62090c71cfe9f
Jacques Hoffmann <jhoffmann@student.ethz.ch>
2026-07-24 12:00:05 +0200

    clearsessions periodically

--- configs/docker-entrypoint.sh
+++ configs/docker-entrypoint.sh
@@ -2,14 +2,7 @@
 
 set -euo pipefail
 
-{
-  cd pgbouncer
-  uv run generate_ini.py
-
-  pgbouncer /dev/shm/pgbouncer.ini
-} &
-
-{
+set_django_env_vars() {
   set -a
   SIP_POSTGRES_DB_SERVER=/dev/shm
   SIP_POSTGRES_DB_PORT=6432
@@ -23,6 +16,17 @@ set -euo pipefail
   ALLOWED_HOSTS="${ALLOWED_HOMEORGS:-ethz.ch}"
   prometheus_multiproc_dir=/dev/shm
   set +a
+}
+
+{
+  cd pgbouncer
+  uv run generate_ini.py
+
+  pgbouncer /dev/shm/pgbouncer.ini
+} &
+
+{
+  set_django_env_vars
 
   uv run manage.py wait_for_database
 
@@ -36,6 +40,15 @@ set -euo pipefail
     --worker-class gevent \
     --worker-tmp-dir /dev/shm \
     --log-level debug
+} &
+
+{
+  set_django_env_vars
+
+  while true; do
+    timeout 10m uv run manage.py clearsessions
+    sleep 1d
+  done
 } &
 
 wait -n
